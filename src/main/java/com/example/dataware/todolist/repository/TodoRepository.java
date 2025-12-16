2aaf0fe411f636742611a9dae5dfd5cee7eae114
miketester10 <cadenza91@gmail.com>
2025-12-16 04:45:38 +0100

    Refactor TodoRepository by commenting out unused imports and query annotations for clarity

--- src/main/java/com/example/dataware/todolist/repository/TodoRepository.java
+++ src/main/java/com/example/dataware/todolist/repository/TodoRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 
 import com.example.dataware.todolist.entity.Todo;
 import com.example.dataware.todolist.entity.User;
@@ -15,6 +15,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     Optional<Todo> findOneByIdAndUser(Long id, User user);
 
-    // @Query(value = "DELETE FROM todos WHERE id = :id AND user_id = :userId", nativeQuery = true)
+    // @Query(value = "DELETE FROM todos WHERE id = :id AND user_id = :userId",
+    // nativeQuery = true)
     // int deleteByIdAndUser(@Param("id") Long id, @Param("userId") Long userId);
 }
